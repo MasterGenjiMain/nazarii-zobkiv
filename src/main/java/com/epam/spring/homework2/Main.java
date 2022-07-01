ab9e2dc678e51b93fe482c259c6b6f78988ed757
Nazarii Zobkiv <nazarii.v.zobkiv@gmail.com>
2022-07-01 15:28:18 +0300

    Fix for finished HW_2

--- src/main/java/com/epam/spring/homework2/Main.java
+++ src/main/java/com/epam/spring/homework2/Main.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.util.Arrays;
 
-
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(PropertiesConfig.class);
