d875049930d4f159f31e2c0c7ee164f50d16eed5
Nazarii Zobkiv <nazarii.v.zobkiv@gmail.com>
2022-07-22 21:21:38 +0300

    fix fox swagger2

--- src/main/java/com/epam/spring/boot/cargodeliverysystem/api/config/SwaggerConfig.java
+++ src/main/java/com/epam/spring/boot/cargodeliverysystem/api/config/SwaggerConfig.java
@@ -6,19 +6,17 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket apiV1() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("api-v1")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.epam.spring.boot.cargodeliverysystem.controller"))
-                .paths(PathSelectors.ant("/api/v1/**"))
+                .paths(PathSelectors.any())
                 .build();
     }
 
